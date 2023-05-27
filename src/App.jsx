91895a3b87bcb87d90f25439f8c4773992b8a79d
Bryan Guerra <bryanmlg2018@gmail.com>
2023-05-27 17:12:27 -0600

    Webpack, eslint, prettier and dev mode config

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Logo from '@assets/AluraIcon.svg';
+function App() {
+  return (
+    <div className='App'>
+      Text
+    </div>
+  );
+}
+
+export default App;
